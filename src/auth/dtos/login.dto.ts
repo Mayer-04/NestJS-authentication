94320fc8106214166a02e9ef27ec1ea8de9e6c09
Mayer Andres <andres.20.chaves@gmail.com>
2024-02-27 18:34:53 -0500

    Tipando DTOs

--- src/auth/dtos/login.dto.ts
+++ src/auth/dtos/login.dto.ts
@@ -1,4 +1,11 @@
+import { IsEmail, IsNotEmpty, Min } from 'class-validator';
+
 export class LoginDto {
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
+  @Min(6)
   password: string;
 }
--- src/auth/dtos/register.dto.ts
+++ src/auth/dtos/register.dto.ts
@@ -1,6 +1,26 @@
+import {
+  Equals,
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
 export class RegisterDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
   password: string;
+
+  @IsString()
+  @Equals('password', { message: 'Passwords do not match' })
   confirmPassword: string;
 }
